1c267fea966c93cc4dbc90aaa40915327efa1a6c
Xavier Cambar <xcambar@gmail.com>
2011-11-02 01:40:31 +0100

    Enhanced the string parser

--- test/grid/import/string-test.js
+++ test/grid/import/string-test.js
@@ -5,12 +5,68 @@ var vows = require('vows'),
 
 vows.describe('Grid String Import').addBatch({
     'invalid import' : {
-        topic: function () {return StringImport;},
+        topic: function () {
+        	return StringImport;
+        },
         'has no parameter' : function (topic) {
-            assert.throws(function () { new topic(); }); 
+            assert.throws(function () { topic(); }); 
         },
         'contains less than 81 elements' : function (topic) {
-            assert.throws(function () { new topic('1,2,3'); }); 
+            assert.throws(function () { topic('1,2,3'); }); 
+        }
+    },
+    'correct import without empty values' : {
+    	topic: function () {
+    		return '1, 2, 3, 4, 5, 6, 7, 8, 9,' +
+	        '4, 5, 6, 7, 8, 9, 1, 2, 3,' +
+	        '7, 8, 9, 1, 2, 3, 4, 5, 6,' +
+	        '2, 3, 4, 5, 6, 7, 8, 9, 1,' +
+	        '5, 6, 7, 8, 9, 1, 2, 3, 4,' +
+	        '8, 9, 1, 2, 3, 4, 5, 6, 7,' +
+	        '3, 4, 5, 6, 7, 8, 9, 1, 2,' +
+	        '6, 7, 8, 9, 1, 2, 3, 4, 5,' +
+	        '9, 1, 2, 3, 4, 5, 6, 7, 8';
+    	},
+        'returns a valid array' : function (topic) {
+        	var expected = [
+    	        1, 2, 3, 4, 5, 6, 7, 8, 9,
+    	        4, 5, 6, 7, 8, 9, 1, 2, 3,
+    	        7, 8, 9, 1, 2, 3, 4, 5, 6,
+    	        2, 3, 4, 5, 6, 7, 8, 9, 1,
+    	        5, 6, 7, 8, 9, 1, 2, 3, 4,
+    	        8, 9, 1, 2, 3, 4, 5, 6, 7,
+    	        3, 4, 5, 6, 7, 8, 9, 1, 2,
+    	        6, 7, 8, 9, 1, 2, 3, 4, 5,
+    	        9, 1, 2, 3, 4, 5, 6, 7, 8
+            ];
+        	assert.deepEqual(StringImport(topic), expected);
+        }
+    },
+    'correct import with empty values' : {
+    	topic: function () {
+    		return '0, 2, 3, 4, 5, 6, 7, 8, 9,' + //Explicit 0
+	        '4, 5, 6, 7, 8, 9, 1, 2, 3,' +
+	        '7, 8, 9, 1, 2, 3, 4, 5, 6,' +
+	        '2, 3, 4, 5, 6, 7, 8, 9, 1,' +
+	        '5, 6, 7, 8, 9, 1, 2, 3, 4,' +
+	        '8, 9, 1, 2, 3, 4, 5, 6, 7,' +
+	        '3, 4, 5, 6, 7, 8, 9, 1, 2,' +
+	        '6, 7, 8, 9, 1, 2, 3,, 5,' +   // Empty value
+	        '9, 1, 2, 3, 4, 5, 6, 7, ';    // Empty Value
+    	},
+        'returns a valid array' : function (topic) {
+        	var expected = [
+    	        null, 2, 3, 4, 5, 6, 7, 8, 9,
+    	        4, 5, 6, 7, 8, 9, 1, 2, 3,
+    	        7, 8, 9, 1, 2, 3, 4, 5, 6,
+    	        2, 3, 4, 5, 6, 7, 8, 9, 1,
+    	        5, 6, 7, 8, 9, 1, 2, 3, 4,
+    	        8, 9, 1, 2, 3, 4, 5, 6, 7,
+    	        3, 4, 5, 6, 7, 8, 9, 1, 2,
+    	        6, 7, 8, 9, 1, 2, 3, null, 5,
+    	        9, 1, 2, 3, 4, 5, 6, 7, null
+            ];
+        	assert.deepEqual(StringImport(topic), expected);
         }
     }
 }).export(module);
